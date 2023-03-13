8753c6e340ebfa43ee440f0ebec564497b8174c2
aseelflihan <aseelflihan@gmail.com>
2023-03-13 13:09:25 +0300

    initialized tailwindcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div class="h-screen flex justify-center items-center">
+    <div class="text-3xl font-bold bg-green-700 text-white-300">
+      hello world
+    </div>
+  </div>
+   )
 }
 
 export default Home
