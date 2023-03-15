739e0f0d4e0342301afa14d049e254bfcd61071b
Hamad Bakeel <90466313+HamadBakeel@users.noreply.github.com>
2023-03-15 19:57:38 +0300

    new: static hero section slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
+import HeroSection from "@/components/HeroSection"
 import Layout from "@/components/Layout"
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-white">
-      <div className="text-3xl font-bold bg-green-700 text-white-300">
-        hello world
-      </div>
-    </div>
+    
+        <HeroSection/>
+    
   )
 }
 
