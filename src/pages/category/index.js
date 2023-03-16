c9c7f0e47f0b9a88d85b83bbae2ef553a7456eb0
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-03-16 19:49:56 +0300

    feat: add filter for all components

--- src/pages/category/index.js
+++ src/pages/category/index.js
@@ -1,14 +1,32 @@
 import axios from 'axios'
+import React, { useState } from 'react'
 import Layout from "@/components/Layout"
 import Category from '@/components/Category'
 
 
 function Home({ categories }) {
+  const [searchQuery, setSearchQuery] = useState('')
+  const [displayedCategories, setDisplayedCategories] = useState(categories.slice(0, 12))
+
+  const handleSearchInput = event => {
+    setSearchQuery(event.target.value)
+    setDisplayedCategories(categories.filter(category => category.name.toLowerCase().includes(event.target.value.toLowerCase())).slice(0, 12))
+  }
+
   return (
     <div className="container mx-auto mb-16">
       <h1 className="my-8 text-3xl font-bold text-white">Categories</h1>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Search products"
+          value={searchQuery}
+          onChange={handleSearchInput}
+          className="w-full px-4 py-2 border border-gray-400 rounded-md"
+        />
+      </div>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {categories.map((category) => (
+        {displayedCategories.map((category) => (
           <Category category={category} />
         ))}
       </div>
--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -1,16 +1,22 @@
+import Head from 'next/head'
 import axios from 'axios'
 import Layout from '@/components/Layout'
 
 export default function Product({ product }) {
     return (
-        <div className="max-w-2xl mx-auto">
-            <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
-            <div className="flex justify-center mb-4">
-                <img src={product.images[0]} alt={product.name} className="max-w-full max-h-80 rounded-lg shadow-md" />
+        <>
+            <Head>
+                <title>{product.title}</title>
+            </Head>
+            <div className="max-w-2xl mx-auto">
+                <h1 className="text-3xl font-bold mb-4">{product.title}</h1>
+                <div className="flex justify-center mb-4">
+                    <img src={product.images[0]} alt={product.name} className="max-w-full max-h-80 rounded-lg shadow-md" />
+                </div>
+                <p className="text-lg mb-4">{product.description}</p>
+                <p className="text-lg font-bold mb-4">${product.price.toFixed(2)}</p>
             </div>
-            <p className="text-lg mb-4">{product.description}</p>
-            <p className="text-lg font-bold mb-4">${product.price.toFixed(2)}</p>
-        </div>
+        </>
     )
 }
 
