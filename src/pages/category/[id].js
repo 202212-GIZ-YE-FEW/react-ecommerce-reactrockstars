fb7d1b27e1eaa0e981296a0c50bc115aac3a17c3
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-03-16 09:37:14 +0300

    feat: add lazy loading

--- src/pages/category/[id].js
+++ src/pages/category/[id].js
@@ -1,7 +1,8 @@
 import { useRouter } from 'next/router'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, lazy, Suspense } from 'react'
 import Layout from "@/components/Layout"
 
+const ProductCard = lazy(() => import('@/components/ProductCard'))
 
 function Category() {
   const router = useRouter()
@@ -26,6 +27,25 @@ function Category() {
       fetchProducts()
     }
   }, [id])
+
+  const [visibleProducts, setVisibleProducts] = useState([])
+  const [loadMoreVisible, setLoadMoreVisible] = useState(true)
+
+  useEffect(() => {
+    if (products.length) {
+      setVisibleProducts(products.slice(0, 8))
+    }
+  }, [products])
+
+  const loadMore = () => {
+    const startIndex = visibleProducts.length
+    const endIndex = startIndex + 8
+    setVisibleProducts([...visibleProducts, ...products.slice(startIndex, endIndex)])
+    if (endIndex >= products.length) {
+      setLoadMoreVisible(false)
+    }
+  }
+
   if (!id) {
     return (
       <div className="flex items-center justify-center h-screen">
@@ -33,6 +53,7 @@ function Category() {
       </div>
     )
   }
+
   if (loading) {
     return (
       <div className="flex items-center justify-center h-screen">
@@ -44,35 +65,30 @@ function Category() {
       </div>
     )
   }
+
   if (!products.length) {
     return (
       <div className="flex items-center justify-center h-screen">
-        <span>No products found for this category.</span>
+        <h1>No products found for this category.</h1>
       </div>
     )
   }
 
   return (
-    <div className="flex flex-wrap justify-center">
-      {products.map((product) => (
-        <div key={product.id} className="w-full px-4 mb-4 sm:w-1/2 md:w-1/3 lg:w-1/4">
-          <div className="overflow-hidden bg-white rounded shadow-lg">
-            <img className="object-cover w-full h-64" src={product.images[0]} alt={product.title} />
-            <div className="px-6 py-4">
-              <div className="mb-2 text-xl font-bold text-gray-900">{product.title}</div>
-              <p className="text-base text-gray-700">{product.description}</p>
-            </div>
-            <div className="px-6 pt-4 pb-2">
-              <span className="inline-block px-3 py-1 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">{product.category.name}</span>
-              <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">{product.price} </span>
-            </div>
-          </div>
-        </div>
+    <div className="flex flex-wrap justify-start p-8">
+      <Suspense fallback={<div>Loading...</div>}>
+      {visibleProducts.map((product) => (
+        <ProductCard key={product.id} product={product} />
       ))}
+      {loadMoreVisible && (
+        <div className="flex items-center justify-center w-full">
+          <button className="px-4 py-2 mt-4 text-white bg-blue-500 rounded hover:bg-blue-600" onClick={loadMore}>Load more</button>
+        </div>
+      )}
+    </Suspense>
     </div>
   )
 }
-
 
 Category.getLayout = function getLayout(page) {
   return (
@@ -81,4 +97,5 @@ Category.getLayout = function getLayout(page) {
     </Layout>
   )
 }
+
 export default Category
